53acf41c1be4d002a5960da0d44f1c67ddb4e9bb
Nicholas Wilson <nwilson@systemsbiology.org>
2015-01-28 10:25:25 -0800

    Test config parser

--- pytest/main.py
+++ pytest/main.py
@@ -3,7 +3,19 @@
 __author__ = 'nwilson'
 
 import sys
+import ConfigParser
+import os
 
 if __name__ == "__main__":
+    config = ConfigParser.ConfigParser()
+    config.read("util/pytest.config")
+
+    print(config.sections())
+
+    print(config.get("category", "option1"))
+    print(config.get("category", "option2"))
+    print(config.get("category", "option3"))
+    print(config.get("category", "option4"))
+
     print(sys.version)
     print("this is only a test")
